8fc1b6c6d8b7ac1684c7b3d2ae4e91774fdc736f
estqwa <altimsco@gmail.com>
2026-01-24 13:11:09 +0500

    обновлениепроекта2 для локального запуска по мелочи, если что откатиться назад

--- migrations/000009_add_reason_to_refresh_tokens.up.sql
+++ migrations/000009_add_reason_to_refresh_tokens.up.sql
@@ -1,5 +1,5 @@
         -- Добавляем колонку для хранения причины отзыва/истечения срока действия
         ALTER TABLE refresh_tokens
-        ADD COLUMN reason TEXT NULL DEFAULT '';
+        ADD COLUMN IF NOT EXISTS reason TEXT NULL DEFAULT '';
 
         COMMENT ON COLUMN refresh_tokens.reason IS 'Причина отзыва или истечения срока действия токена (например, user_logout)';
